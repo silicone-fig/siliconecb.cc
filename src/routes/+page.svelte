480431a6f43bf4eef94c2e33321e8949c41fd2f6
silicone-fig <me@siliconecb.cc>
2024-07-21 01:18:33 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<sveltekit:head>
+  <title>home (beta) | siliconeCB_</title>
+  <link rel="icon" href="https://u.cubeupload.com/siliconefig/picture2.png"/>
+  <meta name="title" content="siliconeCB_ - amateur web dev"/>
+  <meta name="description" content="front-End dev, based in the US 🦅"/>
+</sveltekit:head>
+
+<div class="flex items-center justify-center min-h-screen bg-black">
+  <div class="flex flex-col w-[90%] max-w-[600px] rounded-lg border border-white md:flex-row">
+    <div class="flex-1 md:order-2">
+      <div class="flex items-center justify-center h-full">
+        <div class="w-12 h-12 bg-white rounded-sm"></div>
+      </div>
+    </div>
+    <div class="flex flex-col justify-between gap-8 p-8">
+      <div>
+        <h1 class="text-4xl font-bold lowercase text-white">hi, i'm siliconeCB_</h1>
+        <p class="text-gray-500 lowercase">frontend developer</p>
+        <p class="mt-4 text-white">hello, world!</p>
+      </div>
+      <div class="flex justify-between w-full">
+        <a class="text-white hover:text-green-500 underline underline-offset-4 lowercase" href="blog">
+          blog
+        </a>
+        <a class="text-white hover:text-green-500 underline underline-offset-4 lowercase" href="projects">
+          projects
+        </a>
+        <a class="text-white hover:text-green-500 underline underline-offset-4 lowercase" href="about">
+          about
+        </a>
+        <a class="text-white hover:text-green-500 underline underline-offset-4 lowercase" href="socials">
+          socials
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap');
+
+  :global(body) {
+    font-family: 'Manrope', sans-serif;
+  }
+</style>
